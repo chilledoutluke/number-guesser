826a01e0fe25c85216e4a4ed6463d9943faa4419
chilledoutluke <lukejames.dean08@gmail.com>
2024-04-21 21:57:22 +0200

    script.js
    -added const generateTarget function
    -added compareGuesses function
    - added updateScore function
    - added advanceRound function

--- script.js
+++ script.js
@@ -2,4 +2,30 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-
+// Write your code below:
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+  };
+  
+  console.log(generateTarget());
+  
+  const compareGuesses = (userGuess, computerGuess, targetGuess) => {
+    const userDifference = Math.abs(targetGuess - userGuess);
+    const computerDifference = Math.abs(targetGuess - computerGuess);
+  
+    if(userGuess <= computerGuess) {
+      return true;
+    } else {
+        return false;
+    }
+  };
+  
+  const updateScore = winner => {
+    if(winner === 'user') {
+      humanScore++;
+    } else if (winner === 'computer') {
+      computerScore++;
+    }
+  }
+  
+  const advanceRound = () => currentRoundNumber++;
